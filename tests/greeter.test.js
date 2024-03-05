ed4537fb36042433dd28ab24471ebe8f781e34ac
Oli Casiano <oliversanchezcasiano@gmail.com>
2024-03-05 10:06:45 +0100

    añadir mocks al test

--- tests/greeter.test.js
+++ tests/greeter.test.js
@@ -1,9 +1,33 @@
-import { Greeter } from "../src/Greeter"
+import { Clock } from '../src/Clock';
+import { Greeter } from '../src/Greeter';
 
-test('saludar por las manhanas ', () => {
-    let greeter = new Greeter()
+jest.mock('../src/Clock');
 
-    let saludo = greeter.greet("Oli")
+test('should greet Buenos días in the morning', () => {
+    // Mocking isMorning() to always return true
+    Clock.mockImplementation(() => ({
+        isMorning: jest.fn(() => true),
+        getCurrentTime: jest.fn(),
+    }));
 
-    expect(saludo).toBe("Buenos dias, Oli")
-})
+    const greeter = new Greeter();
+    const result = greeter.greet('John');
+
+    expect(result).toBe('Buenos días, John');
+});
+
+test('should greet Ya son las 17:00', () => {
+    // Mocking isMorning() to always return true
+    Clock.mockImplementation(() => ({
+        isMorning: jest.fn(() => false),
+        getCurrentTime: jest.fn(() => "17:00"),
+    }));
+
+    const greeter = new Greeter();
+    const result = greeter.greet('John');
+
+    expect(result).toBe('Ya son las 17:00, John');
+});
+
+
+
